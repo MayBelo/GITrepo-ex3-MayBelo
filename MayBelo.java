4002e9a57c83cbf57ae00a4f626c3edba243b22f
MayBelo <Maybelo2000@gmail.com>
2025-11-23 01:36:49 +0200

    changed MayBelo.java file

--- MayBelo.java
+++ MayBelo.java
@@ -1,7 +1,7 @@
 public class MayBelo {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            System.out.println("May Belo " + i);
+            System.out.println("May Belo :) " + i);
         }
     }
 }
